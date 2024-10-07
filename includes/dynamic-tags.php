8f63081bb828ce9552091fbd092ca8259bdc282c
Michael Tuozzo <mtuozzo86@gmail.com>
2024-10-07 14:05:59 -0400

    updated includes/dynamic-tags.php to include GMB correction

--- includes/dynamic-tags.php
+++ includes/dynamic-tags.php
@@ -20,7 +20,7 @@ add_action('elementor/dynamic_tags/register', function($dynamic_tags) {
         ['angies-list', "Angi's List", 'URL'],
         ['home-advisor', 'Home Advisor', 'URL'],
         ['business-hours', 'Business Hours', 'TEXT'],
-        ['gmb-link', 'GMB Link', 'URL'],
+        ['gmb-link', 'GMB Link', 'URL'],  // GMB Link added here
         ['facebook', 'Facebook', 'URL'],
         ['instagram', 'Instagram', 'URL'],
         ['twitter', 'Twitter', 'URL'],
@@ -41,56 +41,58 @@ add_action('elementor/dynamic_tags/register', function($dynamic_tags) {
                     public function get_categories() { return [\\Elementor\\Modules\\DynamicTags\\Module::" . ($tag[2] === 'URL' ? 'URL_CATEGORY' : 'TEXT_CATEGORY') . "]; }
                     protected function register_controls() {}
                     public function render() {
-                        if ('company-phone-1' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url('tel:' . esc_attr(get_option('hozio_company_phone_1')));
-                        } elseif ('phone-number-icon-box' === '" . esc_attr($tag[0]) . "') {
-                            echo '<a href=\"tel:' . esc_attr(get_option('hozio_company_phone_1')) . '\">' . esc_html(get_option('hozio_company_phone_1')) . '</a>';
-                        } elseif ('company-phone-1-name' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_html(get_option('hozio_company_phone_1'));
-                        } elseif ('company-phone-2' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url('tel:' . esc_attr(get_option('hozio_company_phone_2')));
-                        } elseif ('company-phone-2-name' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_html(get_option('hozio_company_phone_2'));
-                        } elseif ('sms-phone' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url('sms:' . esc_attr(get_option('hozio_sms_phone')));
-                        } elseif ('sms-icon-box' === '" . esc_attr($tag[0]) . "') {
-                            echo '<a href=\"sms:' . esc_attr(get_option('hozio_sms_phone')) . '\">' . esc_html(get_option('hozio_sms_phone')) . '</a>';
-                        } elseif ('sms-phone-name' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_html(get_option('hozio_sms_phone'));
-                        } elseif ('company-email' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url('mailto:' . esc_attr(get_option('hozio_company_email')));
-                        } elseif ('email-icon-box' === '" . esc_attr($tag[0]) . "') {
-                            echo '<a href=\"mailto:' . esc_attr(get_option('hozio_company_email')) . '\">' . esc_html(get_option('hozio_company_email')) . '</a>';
-                        } elseif ('to-email-contact-form' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_html(get_option('hozio_company_email'));
-                        } elseif ('sitemap-xml' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('home_url') . '/sitemap.xml');
-                        } elseif ('company-address' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_html(get_option('hozio_company_address'));
+                        if ('company-address' === '" . esc_attr($tag[0]) . "') {
+                            echo wp_kses_post(get_option('hozio_company_address'));  // Allow HTML for company address
                         } elseif ('business-hours' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_html(get_option('hozio_business_hours'));
+                            echo wp_kses_post(get_option('hozio_business_hours'));  // Allow HTML for business hours
+                        } elseif ('gmb-link' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_url(get_option('hozio_gmb_link'));  // GMB Link
+                        } elseif ('company-phone-1' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_url('tel:' . esc_attr(get_option('hozio_company_phone_1')));  // Company Phone 1 (URL)
+                        } elseif ('phone-number-icon-box' === '" . esc_attr($tag[0]) . "') {
+                            echo '<a href=\"tel:' . esc_attr(get_option('hozio_company_phone_1')) . '\">' . esc_html(get_option('hozio_company_phone_1')) . '</a>';  // Phone number icon box
+                        } elseif ('company-phone-1-name' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_html(get_option('hozio_company_phone_1'));  // Company phone 1 name
+                        } elseif ('company-phone-2' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_url('tel:' . esc_attr(get_option('hozio_company_phone_2')));  // Company Phone 2 (URL)
+                        } elseif ('company-phone-2-name' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_html(get_option('hozio_company_phone_2'));  // Company phone 2 name
+                        } elseif ('sms-phone' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_url('sms:' . esc_attr(get_option('hozio_sms_phone')));  // SMS phone (URL)
+                        } elseif ('sms-icon-box' === '" . esc_attr($tag[0]) . "') {
+                            echo '<a href=\"sms:' . esc_attr(get_option('hozio_sms_phone')) . '\">' . esc_html(get_option('hozio_sms_phone')) . '</a>';  // SMS phone icon box
+                        } elseif ('sms-phone-name' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_html(get_option('hozio_sms_phone'));  // SMS phone name
+                        } elseif ('company-email' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_url('mailto:' . esc_attr(get_option('hozio_company_email')));  // Company email (URL)
+                        } elseif ('email-icon-box' === '" . esc_attr($tag[0]) . "') {
+                            echo '<a href=\"mailto:' . esc_attr(get_option('hozio_company_email')) . '\">' . esc_html(get_option('hozio_company_email')) . '</a>';  // Email icon box
+                        } elseif ('to-email-contact-form' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_html(get_option('hozio_company_email'));  // To email contact form
+                        } elseif ('sitemap-xml' === '" . esc_attr($tag[0]) . "') {
+                            echo esc_url(get_option('home_url') . '/sitemap.xml');  // Sitemap
                         } elseif ('yelp' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_yelp_url'));
+                            echo esc_url(get_option('hozio_yelp_url'));  // Yelp URL
                         } elseif ('youtube' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_youtube_url'));
+                            echo esc_url(get_option('hozio_youtube_url'));  // YouTube URL
                         } elseif ('angies-list' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_angies_list_url'));
+                            echo esc_url(get_option('hozio_angies_list_url'));  // Angie's List URL
                         } elseif ('home-advisor' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_home_advisor_url'));
+                            echo esc_url(get_option('hozio_home_advisor_url'));  // Home Advisor URL
                         } elseif ('bbb' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_bbb_url'));
+                            echo esc_url(get_option('hozio_bbb_url'));  // BBB URL
                         } elseif ('facebook' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_facebook_url'));
+                            echo esc_url(get_option('hozio_facebook_url'));  // Facebook URL
                         } elseif ('instagram' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_instagram_url'));
+                            echo esc_url(get_option('hozio_instagram_url'));  // Instagram URL
                         } elseif ('twitter' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_twitter_url'));
+                            echo esc_url(get_option('hozio_twitter_url'));  // Twitter URL
                         } elseif ('tiktok' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_tiktok_url'));
+                            echo esc_url(get_option('hozio_tiktok_url'));  // TikTok URL
                         } elseif ('linkedin' === '" . esc_attr($tag[0]) . "') {
-                            echo esc_url(get_option('hozio_linkedin_url'));
+                            echo esc_url(get_option('hozio_linkedin_url'));  // LinkedIn URL
                         } else {
-                            echo esc_html(get_option('hozio_" . str_replace('-', '_', $tag[0]) . "'));
+                            echo wp_kses_post(get_option('hozio_" . str_replace('-', '_', $tag[0]) . "'));  // Default to allow HTML output for all others
                         }
                     }
                 }
