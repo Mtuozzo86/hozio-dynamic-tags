b296bb874046a09c3cb50f7725b66a398b02c576
Michael Tuozzo <77456580+Mtuozzo86@users.noreply.github.com>
2024-11-15 14:33:07 -0500

    Update hozio-dynamic-tags.php

--- hozio-dynamic-tags.php
+++ hozio-dynamic-tags.php
@@ -3,7 +3,7 @@
 Plugin Name: Hozio Dynamic Tags
 Plugin URI: https://github.com/Mtuozzo86/hozio-dynamic-tags
 Description: Adds custom dynamic tags for Elementor to manage Hozio's contact information.
-Version: 3.13.6
+Version: 3.13.5
 Author: Hozio Web Dev
 Author URI: https://github.com/Mtuozzo86/hozio-dynamic-tags
 License: GPL2
@@ -21,156 +21,6 @@ require_once plugin_dir_path(__FILE__) . 'includes/admin-settings.php';
 require_once plugin_dir_path(__FILE__) . 'includes/dynamic-tags.php';
 require_once plugin_dir_path(__FILE__) . 'includes/service-menu-handler.php';
 require_once plugin_dir_path(__FILE__) . 'includes/custom-permalink.php';
-
-// Add the custom admin menu
-function hozio_dynamic_tags_menu() {
-    add_menu_page(
-        'Hozio Dynamic Tags Settings',
-        'Hozio Dynamic Tags',
-        'manage_options',
-        'hozio_dynamic_tags',
-        'hozio_dynamic_tags_contact_info',
-        plugins_url('assets/hozio-logo.png', __FILE__),
-        25
-    );
-
-    add_submenu_page(
-        'hozio_dynamic_tags',
-        'Add / Remove Dynamic Tags',
-        'Add / Remove',
-        'manage_options',
-        'hozio-add-remove-tags',
-        'hozio_add_remove_tags_page'
-    );
-
-    add_submenu_page(
-        'hozio_dynamic_tags',
-        'Custom Permalink Settings',
-        'Blog Permalink Settings',
-        'manage_options',
-        'hozio-permalink-settings',
-        'hozio_permalink_settings_html'
-    );
-}
-
-add_action('admin_menu', 'hozio_dynamic_tags_menu');
-
-// Add a settings link under the plugin details
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'hozio_dynamic_tags_action_links');
-function hozio_dynamic_tags_action_links($links) {
-    $settings_link = '<a href="admin.php?page=hozio_dynamic_tags">Settings</a>';
-    array_unshift($links, $settings_link);
-    return $links;
-}
-
-// Function to display the settings page
-function hozio_dynamic_tags_contact_info() {
-    hozio_dynamic_tags_settings_page();
-}
-
-// Function for displaying the Add/Remove page content
-function hozio_add_remove_tags_page() {
-    include plugin_dir_path(__FILE__) . 'includes/add-remove-tags.php';
-}
-
-// Handle the add tag form submission
-add_action('admin_post_hozio_add_tag', 'hozio_add_dynamic_tag');
-function hozio_add_dynamic_tag() {
-    if (!current_user_can('manage_options') || !wp_verify_nonce($_POST['_wpnonce'], 'hozio_add_tag_nonce')) {
-        wp_die('Unauthorized request');
-    }
-
-    $tag_title = sanitize_text_field($_POST['tag_title']);
-    $tag_type = sanitize_text_field($_POST['tag_type']);
-    $tag_value = sanitize_title($tag_title);
-
-    $custom_tags = get_option('hozio_custom_tags', []);
-    $custom_tags[] = [
-        'title' => $tag_title,
-        'value' => $tag_value,
-        'type' => $tag_type
-    ];
-
-    update_option('hozio_custom_tags', $custom_tags);
-    wp_redirect(admin_url('admin.php?page=hozio-add-remove-tags'));
-    exit;
-}
-
-// Handle tag removal
-add_action('admin_post_hozio_remove_tag', 'hozio_remove_dynamic_tag');
-function hozio_remove_dynamic_tag() {
-    if (!current_user_can('manage_options')) {
-        wp_die('Unauthorized request');
-    }
-
-    $tag_value = sanitize_text_field($_GET['tag']);
-    $custom_tags = get_option('hozio_custom_tags', []);
-
-    foreach ($custom_tags as $key => $tag) {
-        if ($tag['value'] === $tag_value) {
-            unset($custom_tags[$key]);
-            break;
-        }
-    }
-
-    update_option('hozio_custom_tags', array_values($custom_tags));
-    wp_redirect(admin_url('admin.php?page=hozio-add-remove-tags'));
-    exit;
-}
-
-// Register the setting to save the enable/disable option for custom permalinks
-add_action('admin_init', 'hozio_custom_permalink_register_setting');
-function hozio_custom_permalink_register_setting() {
-    register_setting('hozio_permalink_settings', 'hozio_custom_permalink_enabled');
-}
-
-// Render the settings page HTML for custom permalinks
-function hozio_permalink_settings_html() {
-    if (!current_user_can('manage_options')) {
-        return;
-    }
-    ?>
-    <div class="wrap">
-        <h1>Custom Permalink Settings</h1>
-        <form method="post" action="options.php">
-            <?php
-            settings_fields('hozio_permalink_settings');
-            do_settings_sections('hozio_permalink_settings');
-            ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">Enable/Disable Custom Blog Permalink</th>
-                    <td>
-                        <input type="checkbox" name="hozio_custom_permalink_enabled" value="1" <?php checked(1, get_option('hozio_custom_permalink_enabled'), true); ?> />
-                    </td>
-                </tr>
-            </table>
-            <p>Enabling this plugin will add the slug "blog" to all posts.</p>
-            <p><strong>Example if enabled:</strong> domain.com/blog/category/post-name</p>
-            <p><strong>Example if disabled:</strong> domain.com/category/post-name</p>
-            <p><em>To remove the category, go to <strong>Settings > Permalinks</strong> and remove "%category%" from the custom structure.</em></p>
-            <?php submit_button(); ?>
-        </form>
-    </div>
-    <?php
-}
-
-// Hook to modify the permalink structure
-add_filter('post_link', 'hozio_custom_post_link', 10, 2);
-function hozio_custom_post_link($permalink, $post) {
-    $is_enabled = get_option('hozio_custom_permalink_enabled');
-
-    if (!$is_enabled || $post->post_type !== 'post') {
-        return $permalink;
-    }
-
-    $categories = get_the_category($post->ID);
-    if (!empty($categories)) {
-        $category = $categories[0]->slug;
-        $permalink = home_url('/blog/' . $category . '/' . $post->post_name . '/');
-    }
-    return $permalink;
-}
 
 // Register custom dynamic tags for Elementor
 add_action('elementor/dynamic_tags/register', function($dynamic_tags) {
@@ -243,9 +93,27 @@ add_action('elementor/dynamic_tags/register', function($dynamic_tags) {
                     protected function register_controls() {}
                     public function render() {
                         if ('company-address' === '" . esc_attr($tag[0]) . "') {
-                            echo wp_kses_post(get_option('hozio_company_address'));
+                            echo wp_kses(get_option('hozio_company_address'), array(
+                                'br' => array(),
+                                'a' => array('href' => array(), 'title' => array()),
+                                'b' => array(),
+                                'i' => array(),
+                                'p' => array(),
+                                'ul' => array(),
+                                'ol' => array(),
+                                'li' => array()
+                            ));
                         } elseif ('business-hours' === '" . esc_attr($tag[0]) . "') {
-                            echo wp_kses_post(get_option('hozio_business_hours'));
+                            echo wp_kses(get_option('hozio_business_hours'), array(
+                                'br' => array(),
+                                'a' => array('href' => array(), 'title' => array()),
+                                'b' => array(),
+                                'i' => array(),
+                                'p' => array(),
+                                'ul' => array(),
+                                'ol' => array(),
+                                'li' => array()
+                            ));
                         } else {
                             echo esc_html(get_option('" . esc_attr($tag[2]) . "'));
                         }
